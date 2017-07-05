2711e90410d95a1dabd4d77e3c3c632f01e4dfdd
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-05 14:27:51 +0700

    temp

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import getTemp from '../api/getTemp';
 
 export default class WeatherForm extends Component {
+    constructor(props) {
+        super(props);
+        this.onGetTemp = this.onGetTemp.bind(this);
+    }
+
+    onGetTemp() {
+        const cityName = this.refs.txtCityName.value;
+        this.refs.txtCityName.value = '';
+        getTemp(cityName)
+        .then(temp => console.log(temp));
+    }
+
     render() {
         return (
             <div>
                 <br />
-                <input type="text" placeholder="Enter city name" />
+                <input type="text" placeholder="Enter city name" ref="txtCityName" />
                 <br /><br />
-                <button>Get Temp</button>
+                <button onClick={this.onGetTemp}>Get Temp</button>
             </div>
         );
     }
